bb9c3812351f0c12a89cecbaac3e09c5742fb66c
Alex Vendt <alexey.vendt@t-systems.com>
2026-03-12 01:37:59 +0300

    Init structure. Created from the Spring Initializr (https://start.spring.io/)

---
+++ src/main/java/ru/studiotg/test_tasks/minesweeper_game_server/MinesweeperGameServerApplication.java
@@ -0,0 +1,13 @@
+package ru.studiotg.test_tasks.minesweeper_game_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinesweeperGameServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinesweeperGameServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/studiotg/test_tasks/minesweeper_game_server/MinesweeperGameServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.studiotg.test_tasks.minesweeper_game_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinesweeperGameServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
