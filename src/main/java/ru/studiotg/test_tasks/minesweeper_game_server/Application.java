1c1f42d18dff6f8a4b3ca6b5549afaad7b4dc3e8
Alex Vendt <alexey.vendt@t-systems.com>
2026-03-16 22:15:00 +0300

    Init commit. Created with the Spring Initializer
    https://start.spring.io/

---
+++ src/main/java/ru/studiotg/test_tasks/minesweeper_game_server/Application.java
@@ -0,0 +1,13 @@
+package ru.studiotg.test_tasks.minesweeper_game_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
